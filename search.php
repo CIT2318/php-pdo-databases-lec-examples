51fe0f00b9342c9a717b43771bec25cd691b946c
mmantle-hud <m.e.mantle@hud.ac.uk>
2019-10-29 16:23:28 +0000

    Update search.php

--- search.php
+++ search.php
@@ -5,12 +5,14 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 </head>
 <body>
-		<p><a href="index.php"><<< Home</a></p>
-	<h1>A Search Facility</h1>
+<p><a href="index.php"><<< Home</a></p>
+<h1>A Search Facility</h1>
+	
 <form action="results.php" method="get">
 <label for="search">Enter a search term:</label>
 <input type="text" name="search" id="search">
 <input type="submit" name="submitBtn" value="Search">
 </form>
+
 </body>
 </html>

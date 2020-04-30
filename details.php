8fc1e3f2506cd0ef32ba53d83ea2946cecfeaed0
mmantle-hud <m.e.mantle@hud.ac.uk>
2020-04-30 17:52:40 +0100

    Update details.php

--- details.php
+++ details.php
@@ -1,6 +1,7 @@
 <?php
 try{
        $conn = new PDO('mysql:host=localhost;dbname=u0123456', 'u0123456', '01jan96');
+	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 catch (PDOException $exception) 
 {
